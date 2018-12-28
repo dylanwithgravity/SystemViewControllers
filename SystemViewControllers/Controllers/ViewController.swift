39a54af947b2e4a808b582f97ad823f50ee113c9
dylanwithgravity <dylanwithgravity@gmail.com>
2018-12-27 21:10:51 -0500

    Implemented presentation of UIAlert, UIImagePicker and MFMailCompose view controllers

--- SystemViewControllers/Controllers/ViewController.swift
+++ SystemViewControllers/Controllers/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import SafariServices
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var shareButton: UIButton!
@@ -39,32 +40,109 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    // MARK: - Activity Controller
     @IBAction func shareButtonTapped(_ sender: UIButton) {
         // Unwrap the imageView's image
         guard let image = imageView.image else { return }
+        
         // Create instance of UIActivityViewController
         let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
         // Place popover over button that triggered the presentation(for iPads)
         activityController.popoverPresentationController?.sourceView = sender
+        
         // Present activityController to user
         present(activityController, animated: true, completion: nil)
     }
     
+    // MARK: - Safari View Controller
     @IBAction func safariButtonTapped(_ sender: UIButton) {
         // Create URL from string
         if let url = URL(string: "http://www.apple.com") {
+            
             // Create instance of SFSafariViewController
             let safariViewController = SFSafariViewController(url: url)
+            
             // Present safariViewController to user
             present(safariViewController, animated: true, completion: nil)
         }
     }
     
-    @IBAction func cameraButtonTapped(_ sender: Any) {
+    // MARK: - UIAlert and UIImagePicker Controller
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        // Create instance of UIImagePickerController
+        let imagePicker = UIImagePickerController()
+        
+        // Set viewController as the delegate
+        imagePicker.delegate = self
+        
+        // Create instance of UIAlertController
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        // Create cancel UIAlertAction object
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        // Add UIAlertAction object to alertController
+        alertController.addAction(cancelAction)
+        
+        // Create camera UIAlertAction object if source is available
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
+                // Set source type to camera
+                imagePicker.sourceType = .camera
+                // Present imagePickerController to user
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            // Add UIAlert action object to alertController
+            alertController.addAction(cameraAction)
+        }
+        
+        
+        // Create photo library UIAlertAction object if source is available
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (UIAlertAction) in
+                // Set source type to photo library
+                imagePicker.sourceType = .photoLibrary
+                // Present imagePicker to user
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            // Add UIAlertAction object to alert Controller
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        // Place popover over button that triggered the presentation(for iPads)
+        alertController.popoverPresentationController?.sourceView = sender
+        
+        // present alertController to user
+        present(alertController, animated: true, completion: nil)
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
     }
     
-    @IBAction func emailButtonTapped(_ sender: Any) {
+    // MARK: - MFMailComposeViewController
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        // Check if user's device can send email
+        guard MFMailComposeViewController.canSendMail() else { return }
+        
+        // Create MFMailViewController instance
+        let mailComposer = MFMailComposeViewController()
+        // Set viewController as delegate
+        mailComposer.mailComposeDelegate = self
+        
+        mailComposer.setToRecipients(["example@example.com"])
+        mailComposer.setSubject("Look at this")
+        mailComposer.setMessageBody("Hello, this is an email from the app I made.", isHTML: false)
+        
+        present(mailComposer, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
